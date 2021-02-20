2e22165079c1e73312eee913f19917dfe94bd4ef
hayashi <lestecha11@gmail.com>
2021-02-20 02:02:37 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:index, :show, :new]
   
   def index
     @tasks = Task.all.order(id: :desc).page(params[:page]).per(25)
